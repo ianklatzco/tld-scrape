7441851d159592cca2d3c54c79d70b031ccebc10
ian klatzco <iklatzco@gmail.com>
2018-11-28 05:17:40 +0000

    about to ditch crappy one

--- main.rb
+++ main.rb
@@ -1,9 +1,30 @@
-# url = 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt'
 require 'telegram/bot'
+require 'open-uri'
+require 'net/http'
+require 'diffy'
+
+$url = "http://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+$stored = nil
 
 class WebhooksController < Telegram::Bot::UpdatesController
   def start!(*)
+
     respond_with :message, text: 'Hello!'
+	response = Net::HTTP.get_response(URI.parse($url)) # => #<Net::HTTPOK 200 OK readbody=true>
+	response = response.body
+	diff_result = Diffy::Diff.new($stored, response)
+
+	# first run
+	if $stored == nil
+		$stored = response
+		printable = response.slice(0,100)
+		respond_with :message, text: "first run, fetched: #{printable}"
+		return
+	end
+
+	if diff_result != nil
+		respond_with :message, text: diff_result
+	end
   end
 end
 
