2edf0be3ec83040a731e2acc1802f8288631691a
ian klatzco <iklatzco@gmail.com>
2019-08-17 00:31:40 -0500

    updoot

--- main.rb
+++ main.rb
@@ -17,13 +17,27 @@ somefile.close
 # open the last read
 $fname = "last.txt"
 handle = File.open($fname, "r")
+# re-opens the stored file every time this file is run.
 $stored = handle.read
 handle.close
+
+class String
+# oh this is how you add methods to the string class, that's cool!
+	def remove_lines(i)
+		split("\n")[i..-1].join("\n")
+	end
+end
 
 def send_thing()
 	response = Net::HTTP.get_response(URI.parse($url))
 	response = response.body
-	diff_result = Diffy::Diff.new($stored, response, :context => 0)
+
+	# diff the stored file against the new response
+	# (strip the dates)
+	stripped_response = response.remove_lines(1)
+	stripped_stored   = $stored.remove_lines(1)
+
+	diff_result = Diffy::Diff.new(stripped_stored, stripped_response, :context => 0)
 
 	# if the file was empty, write the response
 	if $stored == ''
@@ -52,12 +66,15 @@ logger = Logger.new(STDOUT)
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
 	my_id = 157625604
-	foo = send_thing()
+	diff_result = send_thing()
 	sendend = lambda { |x="hello world"|
 		# i feel like i'm writing INCREDIBLY shitty haskell
-		if x == '' or nil ; return ; end
-		bot.api.send_message(chat_id: my_id, text: x)
+		if x == '' or nil
+			bot.api.send_message(chat_id: my_id, text: "no new TLDs today! have a good night c:")
+		else
+			bot.api.send_message(chat_id: my_id, text: x)
+		end
 	}
 
-	sendend.call(foo)
+	sendend.call(diff_result)
 end
